8cbfd79cd407a1b616112207455c0524438ed59b
florian lahitte <lahitte.florian@gmail.com>
2019-07-19 18:17:18 -0400

    add surveylist

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -3,14 +3,33 @@ import { connect } from 'react-redux';
 import { fetchSurveys } from '../../actions';
 
 class SurveyList extends React.Component {
-
     componentDidMount(){
         this.props.fetchSurveys();
+    }
+
+    renderSurveys(){
+        return this.props.surveys.reverse().map(survey =>{
+            return (
+                <div className="card blue-grey darken-1" key={survey._id}>
+                    <div className="card-content white-text">
+                        <span className="card-title">{survey.title}</span>
+                        <p>{survey.body}</p>
+                        <p className="right">
+                            Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+                        </p>
+                        <div className="card-action">
+                            <p>Yes: {survey.yes}</p>
+                            <p>Yes: {survey.No}</p>
+                        </div>
+                    </div>
+                </div>
+            )
+        })
     }
     render(){
         return(
             <div>
-                SurveyList
+                {this.renderSurveys()}
             </div>
         )
     }
